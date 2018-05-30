f4b5e829814d048584c59f34f1814a4185d00cbd
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-05-30 12:18:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ function addElementToBeginningOfArray(array,element) {
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return [element(unshift),...array]
+  return array.unshift(element)
 }
 function addElementToEndOfArray(array,element) {
   return [...array, element]
