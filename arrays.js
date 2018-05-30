5c1e0546802b700b5841dc00a8213d5a60dc3f2c
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-05-30 12:43:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function removeElementFromBeginningOfArray(array) {
   return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+  array.slice()
   return array;
 }
 function removeElementFromEndOfArray(array) {
