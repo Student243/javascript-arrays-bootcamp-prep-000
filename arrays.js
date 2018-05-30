c349d6c37d2b1016d35e3654f95ec2b6903ffe03
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-05-30 12:29:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function accessElementInArray(array,index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice()
